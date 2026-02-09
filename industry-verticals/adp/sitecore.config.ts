c5eea49afad59bdc6f62f868930751fdeea9f6e1
sc-johanbecue <johan.becue@sitecore.com>
2026-02-09 17:08:34 +0100

    modified:   industry-verticals/adp/sitecore.config.ts

--- industry-verticals/adp/sitecore.config.ts
+++ industry-verticals/adp/sitecore.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from '@sitecore-content-sdk/nextjs/config';
  * See the documentation for `defineConfig`:
  * https://doc.sitecore.com/xmc/en/developers/content-sdk/the-sitecore-configuration-file.html
  */
-
-export default defineConfig({
-  sitecoreInternalEditingHostUrl: 'http://localhost:3000',
-});
+export default defineConfig({});
+// export default defineConfig({
+//   sitecoreInternalEditingHostUrl: 'http://localhost:3000',
+// });
